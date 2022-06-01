6459e19b30d88036bcf350acee59da18ed1b45c7
Azat Wright <azatwright@gmail.com>
2022-06-01 23:33:32 +0000

    record: make lib.s treewide

---
+++ lib.s
@@ -0,0 +1,103 @@
+.include "linux_64.s"
+
+.section .text
+
+# registers used:
+#
+# r12  number
+# r13  s
+# r14  i
+#
+.type itoa, @function
+.globl itoa
+.set _n, 16
+.set _s, 24
+.set _len, -8
+.set _nabs, -16
+itoa:
+	push %rbp
+	mov %rsp, %rbp
+	sub $16, %rsp
+
+	mov _n(%rbp), %r12
+	mov _s(%rbp), %r13
+	mov $0, %r14
+
+	movq $0, _len(%rbp)
+	mov %r12, _nabs(%rbp)
+
+	cmp $0, %r12
+	jge itoaLoop1
+
+	movb $'-', (%r13, %r14, 1)
+	incq _len(%rbp)
+	imul $-1, %r12
+	mov %r12, _nabs(%rbp)
+
+itoaLoop1:
+	mov $0, %rdx
+	mov %r12, %rax
+	mov $10, %rbx
+	idiv %rbx
+
+	incq _len(%rbp)
+
+	mov %rax, %r12
+	cmp $0, %r12
+	jne itoaLoop1
+
+	mov _nabs(%rbp), %r12
+	mov _len(%rbp), %r14
+	dec %r14
+
+itoaLoop2:
+	mov $0, %rdx
+	mov %r12, %rax
+	mov $10, %rbx
+	idiv %rbx
+
+	mov %dl, (%r13, %r14, 1)
+	add $'0', (%r13, %r14, 1)
+	dec %r14
+
+	mov %rax, %r12
+	cmp $0, %r12
+	jne itoaLoop2
+
+	mov _len(%rbp), %rax
+
+	mov %rbp, %rsp
+	pop %rbp
+	ret
+
+# registers used:
+#
+# rax  length of the string, returned
+# r12  string address, preserved
+#
+.type strlen, @function
+.globl strlen
+.set _s, 16
+strlen:
+	push %rbp
+	mov %rsp, %rbp
+
+	# preserve registers
+	push %r12
+
+	mov $0, %rax
+	mov _s(%rbp), %r12
+strlenLoop:
+	cmpb $0, (%r12, %rax, 1)
+	je strlenLoopEnd
+	inc %rax
+	jmp strlenLoop
+
+strlenLoopEnd:
+
+	# restore registers
+	pop %r12
+
+	mov %rbp, %rsp
+	pop %rbp
+	ret
