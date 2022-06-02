f6cd5806c3f4bd8003c069dbd52856397857daad
Azat Wright <azatwright@gmail.com>
2022-06-02 02:27:48 +0000

    power.s: use stack in a more disciplined way

--- power.s
+++ power.s
@@ -1,21 +1,27 @@
 .section .text
-.globl _start
 
+.globl _start
+.set _sum, -8
 _start:
+	mov %rsp, %rbp
+	sub $8, %rsp
+
+	movq $0, _sum(%rbp)
+
 	push $3 # power
 	push $2 # base
 	call lib_power
 	add $16, %rsp
-	push %rax
+	
+	add %rax, _sum(%rbp)
 
 	push $4 # power
 	push $3 # base
 	call lib_power
 	add $16, %rsp
 
-	# sum
-	add %rax, (%rsp)
+	add %rax, _sum(%rbp)
 
-	mov $60,    %rax # SYS_exit
-	mov (%rsp), %rdi # status
+	mov $60,        %rax # SYS_exit
+	mov _sum(%rbp), %rdi # status
 	syscall
